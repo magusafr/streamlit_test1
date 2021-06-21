d2152f95e29cca6ba74c157b89db42e8a54fb24a
magusafr <40602197+magusafr@users.noreply.github.com>
2021-06-21 10:12:27 +0700

    Update covid_realtime.py

--- covid_realtime.py
+++ covid_realtime.py
@@ -23,4 +23,4 @@ new_cases['date'] = tgl_trx2(new_cases['date'])
 new_cases = new_cases.fillna(0)
 df = new_cases.set_index('date')
 
-st.line_chart(df['new_cases'], width=400, height=200, use_container_width=True)
+st.line_chart(df['new_cases'], width=200, height=100, use_container_width=True)
